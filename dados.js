fccb5dbe1d34fab53ef4c0419e2e349a8cd79fda
Andreo Almada (Andy) <142360767+AndreoAlmada@users.noreply.github.com>
2024-09-06 20:53:47 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        título : "Rayssa Leal",
+        descricao : "Rayssa Leal : A fadinha do skate brasileiro Rayssa Leal é uma skatista brasileira que conquistou o mundo com seu talento precoce e carisma. Aos poucos anos de idade, ela já coleciona títulos e se tornou um ícone do skate feminino. Sua história inspiradora e suas manobras incríveis a tornaram uma das atletas mais admiradas do país.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags:"skate fada sk8"
+    },
+    
+        {
+            título: "Beatriz Souza",
+            descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é considerada uma das maiores promessas do judô brasileiro. Sua história de superação e dedicação à modalidade a tornou uma inspiração para muitos jovens atletas.",
+            link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+            tags:"judô judo judoca judoka" 
+        },
+        {
+            título: "Rebeca Andrade",
+            descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e beleza. Ela é a primeira ginasta brasileira a conquistar uma medalha de ouro olímpica individual. Sua história de superação após uma grave lesão a tornou um símbolo de força e determinação.",
+            link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+            tags: "ginástica ginastica ouro"
+        }
+    ];
+
